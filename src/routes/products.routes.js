ea56058b10066d379627cbb0c2ef3c8358f478ad
dianamutinda <jaydiana25@gmail.com>
2024-06-26 13:53:25 +0300

    implements the working of update route

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -19,7 +19,11 @@ router.get("/:id", async (req, res) => {
     const product = await prisma.product.findFirst({
       where: {productId: id}
     });
-    res.status(200).json(product)
+    if (!product){
+      res.status(404).json({success: false, message:"product not found"})
+    }else {
+      res.status(200).json(product)
+    }
   } catch (error) {
     res.status(500).json({success: false, message:error.message})
   }
@@ -43,8 +47,49 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.patch("/:id", (req, res) => {
-  res.send("updating a product");
+router.patch("/:id", async(req, res) => {
+  const { productThumbnail, productTitle,  productDescription, productCost, onOffer } = req.body;
+  const id = req.params.id;
+  try {
+    let updateProduct;
+    if (productThumbnail){
+      updateProduct = await prisma.product.update({
+        where: {productId: id},
+        data: {productThumbnail:productThumbnail}
+      })
+    }
+
+    if (productTitle){
+      updateProduct = await prisma.product.update({
+        where: {productId: id},
+        data: {productTitle:productTitle}
+      })
+    }
+
+    if (productDescription){
+      updateProduct = await prisma.product.update({
+        where: {productId: id},
+        data: {productDescription:productDescription}
+      })
+    }
+
+    if (productCost){
+      updateProduct = await prisma.product.update({
+        where: {productId: id},
+        data: {productCost:productCost}
+      })
+    }
+
+    if (onOffer){
+      updateProduct = await prisma.product.update({
+        where: {productId: id},
+        data: {onOffer:onOffer}
+      })
+    }
+    res.status(200).json(updateProduct)
+  } catch (error) {
+    res.status(500).json({success: false, message:error.message})
+  }
 });
 
 router.delete("/:id", async(req, res) => {
@@ -53,7 +98,11 @@ router.delete("/:id", async(req, res) => {
     const product = await prisma.product.delete({
       where: {productId: id}
     });
-    res.status(200).json(product)
+    if (!product){
+      res.status(404).json({success: false, message:"product not found"})
+    }else {
+      res.status(200).json(product)
+    }
   } catch (error) {
     res.status(500).json({success: false, message:error.message})
   }
